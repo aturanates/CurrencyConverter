29b1fbb455429312188c2068217987b1c11dfecc
turan ates <aturnaatess@gmail.com>
2025-05-28 09:22:31 +0300

    Add project files.

---
+++ RapidAPICurrency/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace RapidAPICurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDolarAlis = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEuroAlis = new System.Windows.Forms.Label();
+            this.lblEuroSatis = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDolarAlis
+            // 
+            this.lblDolarAlis.AutoSize = true;
+            this.lblDolarAlis.Location = new System.Drawing.Point(58, 41);
+            this.lblDolarAlis.Name = "lblDolarAlis";
+            this.lblDolarAlis.Size = new System.Drawing.Size(44, 16);
+            this.lblDolarAlis.TabIndex = 0;
+            this.lblDolarAlis.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // lblEuroAlis
+            // 
+            this.lblEuroAlis.AutoSize = true;
+            this.lblEuroAlis.Location = new System.Drawing.Point(175, 41);
+            this.lblEuroAlis.Name = "lblEuroAlis";
+            this.lblEuroAlis.Size = new System.Drawing.Size(44, 16);
+            this.lblEuroAlis.TabIndex = 2;
+            this.lblEuroAlis.Text = "label3";
+            // 
+            // lblEuroSatis
+            // 
+            this.lblEuroSatis.AutoSize = true;
+            this.lblEuroSatis.Location = new System.Drawing.Point(175, 90);
+            this.lblEuroSatis.Name = "lblEuroSatis";
+            this.lblEuroSatis.Size = new System.Drawing.Size(44, 16);
+            this.lblEuroSatis.TabIndex = 3;
+            this.lblEuroSatis.Text = "label4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unit Price:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 247);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(138, 207);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 20);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dolar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(260, 207);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 20);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Euro";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblDolarAlis);
+            this.groupBox1.Controls.Add(this.lblEuroAlis);
+            this.groupBox1.Controls.Add(this.lblEuroSatis);
+            this.groupBox1.Location = new System.Drawing.Point(172, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 152);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(273, 276);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Amount to be Paid:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(371, 207);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 20);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Euro";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 472);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDolarAlis;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEuroAlis;
+        private System.Windows.Forms.Label lblEuroSatis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ RapidAPICurrency/Form1.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace RapidAPICurrency
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient;
+        private Dictionary<string, string> currencyNames;
+        private DateTime lastUpdateTime;
+        private bool isConverting = false;
+
+        
+        private ComboBox cmbFromCurrency;
+        private ComboBox cmbToCurrency;
+        private TextBox txtFromAmount;
+        private TextBox txtToAmount;
+        private Label lblResult;
+        private Label lblUpdateTime;
+        private Label lblFromFlag;
+        private Label lblToFlag;
+        private Label lblFromName;
+        private Label lblToName;
+        private Button btnConvert;
+        private Button btnSwap;
+        private ProgressBar progressBar;
+        private Panel resultPanel;
+
+        public Form1()
+        {
+            InitializeComponent();
+            httpClient = new HttpClient();
+            InitializeCurrencyNames();
+            SetupModernUI();
+        }
+
+        private void InitializeCurrencyNames()
+        {
+            currencyNames = new Dictionary<string, string>
+            {
+                {"USD", "US Dollar"},
+                {"EUR", "Euro"},
+                {"GBP", "British Pound"},
+                {"TRY", "Turkish Lira"},
+                {"JPY", "Japanese Yen"},
+                {"CHF", "Swiss Franc"},
+                {"CAD", "Canadian Dollar"},
+                {"AUD", "Australian Dollar"},
+                {"CNY", "Chinese Yuan"},
+                {"SEK", "Swedish Krona"},
+                {"NOK", "Norwegian Krone"},
+                {"DKK", "Danish Krone"},
+                {"PLN", "Polish Zloty"},
+                {"CZK", "Czech Koruna"},
+                {"HUF", "Hungarian Forint"},
+                {"RUB", "Russian Ruble"},
+                {"INR", "Indian Rupee"},
+                {"BRL", "Brazilian Real"},
+                {"ZAR", "South African Rand"},
+                {"KRW", "South Korean Won"}
+            };
+        }
+
+        private void SetupModernUI()
+        {
+            
+            this.Size = new Size(500, 600);
+            this.MinimumSize = new Size(480, 550);
+            this.Text = "Modern Döviz Çevirici";
+            this.BackColor = Color.FromArgb(240, 242, 245);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            
+            this.Controls.Clear();
+
+            Panel mainPanel = new Panel
+            {
+                Dock = DockStyle.Fill,
+                Padding = new Padding(20),
+                BackColor = Color.Transparent
+            };
+
+            Label titleLabel = new Label
+            {
+                Text = "💱 Döviz Çevirici",
+                Font = new Font("Segoe UI", 18, FontStyle.Bold),
+                ForeColor = Color.FromArgb(51, 51, 51),
+                AutoSize = true,
+                Location = new Point(20, 20)
+            };
+
+            lblUpdateTime = new Label
+            {
+                Text = "Son güncelleme: -",
+                Font = new Font("Segoe UI", 9),
+                ForeColor = Color.FromArgb(128, 128, 128),
+                AutoSize = true,
+                Location = new Point(20, 55)
+            };
+
+            GroupBox fromGroup = CreateFromCurrencyGroup();
+
+            GroupBox toGroup = CreateToCurrencyGroup();
+
+            btnSwap = new Button
+            {
+                Text = "⇅",
+                Size = new Size(50, 30),
+                Location = new Point(225, 195),
+                Font = new Font("Segoe UI", 14, FontStyle.Bold),
+                BackColor = Color.FromArgb(52, 152, 219),
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                Cursor = Cursors.Hand
+            };
+            btnSwap.FlatAppearance.BorderSize = 0;
+            btnSwap.Click += BtnSwap_Click;
+
+            btnConvert = new Button
+            {
+                Text = "💰 Çevir",
+                Size = new Size(200, 45),
+                Location = new Point(150, 360),
+                Font = new Font("Segoe UI", 12, FontStyle.Bold),
+                BackColor = Color.FromArgb(46, 204, 113),
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                Cursor = Cursors.Hand
+            };
+            btnConvert.FlatAppearance.BorderSize = 0;
+            btnConvert.Click += BtnConvert_Click;
+
+            resultPanel = new Panel
+            {
+                Size = new Size(440, 80),
+                Location = new Point(20, 420),
+                BackColor = Color.White,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            lblResult = new Label
+            {
+                Text = "Sonuç burada görünecek...",
+                Font = new Font("Segoe UI", 12),
+                ForeColor = Color.FromArgb(51, 51, 51),
+                AutoSize = false,
+                Size = new Size(420, 60),
+                Location = new Point(10, 10),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+
+            resultPanel.Controls.Add(lblResult);
+
+            progressBar = new ProgressBar
+            {
+                Size = new Size(440, 5),
+                Location = new Point(20, 505),
+                Style = ProgressBarStyle.Marquee,
+                MarqueeAnimationSpeed = 30,
+                Visible = false
+            };
+
+            mainPanel.Controls.AddRange(new Control[] {
+                titleLabel, lblUpdateTime, fromGroup, toGroup,
+                btnSwap, btnConvert, resultPanel, progressBar
+            });
+
+            this.Controls.Add(mainPanel);
+        }
+
+        private GroupBox CreateFromCurrencyGroup()
+        {
+            GroupBox group = new GroupBox
+            {
+                Text = "Çevrilecek Para Birimi",
+                Size = new Size(440, 100),
+                Location = new Point(20, 90),
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                ForeColor = Color.FromArgb(51, 51, 51)
+            };
+
+            txtFromAmount = new TextBox
+            {
+                Size = new Size(120, 25),
+                Location = new Point(15, 30),
+                Font = new Font("Segoe UI", 11),
+                Text = "1"
+            };
+            txtFromAmount.TextChanged += TxtFromAmount_TextChanged;
+            txtFromAmount.KeyPress += AmountBox_KeyPress;
+
+            cmbFromCurrency = new ComboBox
+            {
+                Size = new Size(280, 25),
+                Location = new Point(145, 30),
+                Font = new Font("Segoe UI", 10),
+                DropDownStyle = ComboBoxStyle.DropDownList
+            };
+
+            foreach (var currency in currencyNames)
+            {
+                cmbFromCurrency.Items.Add($"{currency.Key} - {currency.Value}");
+            }
+            cmbFromCurrency.SelectedIndex = currencyNames.Keys.ToList().IndexOf("EUR");
+            cmbFromCurrency.SelectedIndexChanged += CmbFromCurrency_SelectedIndexChanged;
+
+            lblFromFlag = new Label
+            {
+                Size = new Size(30, 25),
+                Location = new Point(15, 60),
+                Font = new Font("Segoe UI", 16),
+                Text = GetCurrencyFlag("EUR"),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+
+            lblFromName = new Label
+            {
+                Size = new Size(280, 25),
+                Location = new Point(55, 60),
+                Font = new Font("Segoe UI", 9),
+                ForeColor = Color.FromArgb(128, 128, 128),
+                Text = currencyNames["EUR"],
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+
+            group.Controls.AddRange(new Control[] { txtFromAmount, cmbFromCurrency, lblFromFlag, lblFromName });
+            return group;
+        }
+
+        private GroupBox CreateToCurrencyGroup()
+        {
+            GroupBox group = new GroupBox
+            {
+                Text = "Hedef Para Birimi",
+                Size = new Size(440, 100),
+                Location = new Point(20, 220),
+                Font = new Font("Segoe UI", 10, FontStyle.Bold),
+                ForeColor = Color.FromArgb(51, 51, 51)
+            };
+
+            txtToAmount = new TextBox
+            {
+                Size = new Size(120, 25),
+                Location = new Point(15, 30),
+                Font = new Font("Segoe UI", 11),
+                Text = "0",
+                ReadOnly = true,
+                BackColor = Color.FromArgb(245, 245, 245)
+            };
+
+            cmbToCurrency = new ComboBox
+            {
+                Size = new Size(280, 25),
+                Location = new Point(145, 30),
+                Font = new Font("Segoe UI", 10),
+                DropDownStyle = ComboBoxStyle.DropDownList
+            };
+
+            foreach (var currency in currencyNames)
+            {
+                cmbToCurrency.Items.Add($"{currency.Key} - {currency.Value}");
+            }
+            cmbToCurrency.SelectedIndex = currencyNames.Keys.ToList().IndexOf("USD");
+            cmbToCurrency.SelectedIndexChanged += CmbToCurrency_SelectedIndexChanged;
+
+            lblToFlag = new Label
+            {
+                Size = new Size(30, 25),
+                Location = new Point(15, 60),
+                Font = new Font("Segoe UI", 16),
+                Text = GetCurrencyFlag("USD"),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+
+            lblToName = new Label
+            {
+                Size = new Size(280, 25),
+                Location = new Point(55, 60),
+                Font = new Font("Segoe UI", 9),
+                ForeColor = Color.FromArgb(128, 128, 128),
+                Text = currencyNames["USD"],
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+
+            group.Controls.AddRange(new Control[] { txtToAmount, cmbToCurrency, lblToFlag, lblToName });
+            return group;
+        }
+
+        private string GetCurrencyFlag(string currencyCode)
+        {
+            switch (currencyCode)
+            {
+                case "USD": return "🇺🇸";
+                case "EUR": return "🇪🇺";
+                case "GBP": return "🇬🇧";
+                case "TRY": return "🇹🇷";
+                case "JPY": return "🇯🇵";
+                case "CHF": return "🇨🇭";
+                case "CAD": return "🇨🇦";
+                case "AUD": return "🇦🇺";
+                case "CNY": return "🇨🇳";
+                case "SEK": return "🇸🇪";
+                case "NOK": return "🇳🇴";
+                case "DKK": return "🇩🇰";
+                case "PLN": return "🇵🇱";
+                case "CZK": return "🇨🇿";
+                case "HUF": return "🇭🇺";
+                case "RUB": return "🇷🇺";
+                case "INR": return "🇮🇳";
+                case "BRL": return "🇧🇷";
+                case "ZAR": return "🇿🇦";
+                case "KRW": return "🇰🇷";
+                default: return "💱";
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ShowProgress(true);
+                await ConvertCurrency();
+                UpdateLastUpdateTime();
+            }
+            finally
+            {
+                ShowProgress(false);
+            }
+        }
+
+        private void ShowProgress(bool show)
+        {
+            if (progressBar != null)
+                progressBar.Visible = show;
+        }
+
+        private void UpdateLastUpdateTime()
+        {
+            lastUpdateTime = DateTime.Now;
+            if (lblUpdateTime != null)
+                lblUpdateTime.Text = $"Son güncelleme: {lastUpdateTime:HH:mm:ss}";
+        }
+
+        private async void BtnConvert_Click(object sender, EventArgs e)
+        {
+            if (isConverting) return;
+
+            try
+            {
+                ShowProgress(true);
+                await ConvertCurrency();
+                UpdateLastUpdateTime();
+            }
+            finally
+            {
+                ShowProgress(false);
+            }
+        }
+
+        private async Task ConvertCurrency()
+        {
+            if (isConverting) return;
+            isConverting = true;
+
+            try
+            {
+                if (!decimal.TryParse(txtFromAmount.Text, out decimal amount) || amount <= 0)
+                {
+                    lblResult.Text = "⚠️ Lütfen geçerli bir miktar girin!";
+                    lblResult.ForeColor = Color.FromArgb(231, 76, 60);
+                    return;
+                }
+
+                string fromCurrency = cmbFromCurrency.SelectedItem.ToString().Split('-')[0].Trim();
+                string toCurrency = cmbToCurrency.SelectedItem.ToString().Split('-')[0].Trim();
+
+                if (fromCurrency == toCurrency)
+                {
+                    txtToAmount.Text = amount.ToString("N4");
+                    lblResult.Text = $"✅ {amount:N4} {fromCurrency} = {amount:N4} {toCurrency}";
+                    lblResult.ForeColor = Color.FromArgb(46, 204, 113);
+                    return;
+                }
+
+                string url = $"https://api.frankfurter.app/latest?from={fromCurrency}&to={toCurrency}";
+
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(responseBody);
+
+                var rates = json["rates"];
+                if (rates[toCurrency] != null)
+                {
+                    decimal rate = (decimal)rates[toCurrency];
+                    decimal convertedAmount = amount * rate;
+
+                    txtToAmount.Text = convertedAmount.ToString("N4");
+                    lblResult.Text = $"✅ {amount:N4} {fromCurrency} = {convertedAmount:N4} {toCurrency}";
+                    lblResult.ForeColor = Color.FromArgb(46, 204, 113);
+                }
+                else
+                {
+                    lblResult.Text = "❌ Döviz kuru bulunamadı!";
+                    lblResult.ForeColor = Color.FromArgb(231, 76, 60);
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"❌ Hata: {ex.Message}";
+                lblResult.ForeColor = Color.FromArgb(231, 76, 60);
+            }
+            finally
+            {
+                isConverting = false;
+            }
+        }
+
+        private void BtnSwap_Click(object sender, EventArgs e)
+        {
+            int tempIndex = cmbFromCurrency.SelectedIndex;
+            cmbFromCurrency.SelectedIndex = cmbToCurrency.SelectedIndex;
+            cmbToCurrency.SelectedIndex = tempIndex;
+        }
+
+        private async void TxtFromAmount_TextChanged(object sender, EventArgs e)
+        {
+            if (!isConverting && !string.IsNullOrEmpty(txtFromAmount.Text))
+            {
+                await Task.Delay(500); 
+                if (!isConverting)
+                    await ConvertCurrency();
+            }
+        }
+
+        private void AmountBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                e.KeyChar != '.' && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+
+            TextBox textBox = sender as TextBox;
+            if ((e.KeyChar == '.' || e.KeyChar == ',') &&
+                (textBox.Text.Contains('.') || textBox.Text.Contains(',')))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private async void CmbFromCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCurrency = cmbFromCurrency.SelectedItem.ToString().Split('-')[0].Trim();
+
+            lblFromFlag.Text = GetCurrencyFlag(selectedCurrency);
+            lblFromName.Text = currencyNames[selectedCurrency];
+
+            if (!isConverting)
+                await ConvertCurrency();
+        }
+
+        private async void CmbToCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedCurrency = cmbToCurrency.SelectedItem.ToString().Split('-')[0].Trim();
+
+            lblToFlag.Text = GetCurrencyFlag(selectedCurrency);
+            lblToName.Text = currencyNames[selectedCurrency];
+
+            if (!isConverting)
+                await ConvertCurrency();
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            httpClient?.Dispose();
+            base.OnFormClosed(e);
+        }
+    }
+}
